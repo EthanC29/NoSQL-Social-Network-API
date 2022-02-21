3f88a9bcc4a709d4b52a6e992f2bf056f1b3197a
Ethan <90936828+EthanC29@users.noreply.github.com>
2022-02-21 01:35:34 -0500

    created GET requests for all users and users by id

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const db = require('./models');
+const { User } = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,8 +22,44 @@ mongoose.set('debug', true);
 
 
 // Routes Here
+// GET all users
+app.get('/api/users', (req, res) => {
+  User.find()
+    .then(dbUserData => {
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// GET a single user by id
+app.get('/api/users/:id', (req, res) => {
+  User.findById({ _id: req.params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        return res.status(404).json({ message: "No user with this id!"});
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST a new user
 
 
+// PUT to update a user by id
+
+
+//DELETE to remove a user by id
+
+
+
+
+// Routes End Here
 
 
 app.listen(PORT, () => {
